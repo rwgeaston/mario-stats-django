aadc105657e1ce76141e60835db0a4cd3170d008
Robert Easton <robert@ueni.com>
2018-07-10 07:41:30 +0100

    allow nested admin

--- app/main/urls.py
+++ app/main/urls.py
@@ -7,4 +7,5 @@ from .api.v1 import v1_router
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'api/v1/', include(v1_router.urls)),
+    url(r'^_nested_admin/', include('nested_admin.urls')),
 ]
